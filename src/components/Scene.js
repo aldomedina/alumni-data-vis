1abce7b9a8ce722e991bfa407fe5545493318275
Aldo <aldo.medina.z@gmail.com>
2023-10-23 18:35:51 -0300

    handle perf

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,5 +1,9 @@
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Environment } from "@react-three/drei";
+import {
+  OrbitControls,
+  Environment,
+  PerformanceMonitor,
+} from "@react-three/drei";
 import { Physics } from "@react-three/cannon";
 import { degToRad } from "three/src/math/MathUtils";
 
@@ -7,16 +11,35 @@ import Pointer from "./Pointer";
 import Grupos from "./Grupos";
 import Tags from "./Tags";
 import useStore from "../store/app";
+import { Suspense, useState } from "react";
 
 const Scene = () => {
   const data = useStore((s) => s.data);
+  const [isHighPerf, setIsHighPerf] = useState(true);
 
   return (
     <Canvas
       shadows
+      dpr={isHighPerf ? 2 : 1}
       camera={{ position: [0, 0, 80], fov: 35, near: 1, far: 1000 }}
     >
-      {/* <ambientLight intensity={0.5} /> */}
+      <PerformanceMonitor
+        onIncline={() => {
+          console.log("high perf");
+          setIsHighPerf(true);
+        }}
+        onDecline={() => {
+          console.log("low perf");
+          setIsHighPerf(false);
+        }}
+      />
+      {isHighPerf ? (
+        <Suspense fallback={<ambientLight intensity={0.5} />}>
+          <Environment files="/cyclorama_hard_light_2k.hdr" />{" "}
+        </Suspense>
+      ) : (
+        <ambientLight intensity={0.5} />
+      )}
       <OrbitControls
         enablePan={false}
         maxPolarAngle={degToRad(130)}
@@ -24,8 +47,6 @@ const Scene = () => {
         maxAzimuthAngle={degToRad(50)}
         minAzimuthAngle={degToRad(-50)}
       />
-      {/* <color attach="background" args={["#dfdfdf"]} /> */}
-      <Environment files="/cyclorama_hard_light_2k.hdr" />
       <Tags />
       <Physics gravity={[0, 2, 0]} iterations={10}>
         <Grupos data={data} />
